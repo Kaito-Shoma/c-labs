3f5bfb159acc1c07ab6edbabeaa351276dcb1784
Kaito-Shoma <60389018+Kaito-Shoma@users.noreply.github.com>
2020-06-06 15:25:46 +0300

    Add files via upload

---
+++ Laba1/cmake-build-debug/CMakeFiles/L1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L1.dir/main.c.obj"
+  "L1.exe"
+  "L1.exe.manifest"
+  "L1.lib"
+  "L1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/L1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
